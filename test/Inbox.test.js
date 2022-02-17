7207287aea3143c53402454de18b2cdeef80ab3e
Mateusz Mężyk <mateuszmezyk90@gmail.com>
2022-02-17 20:02:20 +0000

    deploying contract to ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,20 +2,29 @@
 const assert = require("assert");
 // ganache is a local test blockchain network
 const ganache = require("ganache-cli");
+const { from } = require("memorystream");
 const Web3 = require("web3");
+const { interface, bytecode } = require("../compile");
 
 const web3 = new Web3(ganache.provider());
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   // Get list of all accounts
   accounts = await web3.eth.getAccounts();
   // Use one of the accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(accounts);
+    console.log(inbox);
   });
 });
