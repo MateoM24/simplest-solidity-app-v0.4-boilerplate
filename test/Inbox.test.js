21bc0ab178efb069e0d2c34e1c37713bf69edbae
Mateusz Mężyk <mateuszmezyk90@gmail.com>
2022-02-17 19:49:11 +0000

    code cleanup, async in place of then

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,14 +6,16 @@ const Web3 = require("web3");
 
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+let accounts;
+
+beforeEach(async () => {
   // Get list of all accounts
-  web3.eth.getAccounts().then((accounts) => {
-    console.log(accounts);
-  });
+  accounts = await web3.eth.getAccounts();
   // Use one of the accounts to deploy the contract
 });
 
 describe("Inbox", () => {
-  it("deploys a contract", () => {});
+  it("deploys a contract", () => {
+    console.log(accounts);
+  });
 });
